6388ceeaa2b8cec0074ecccfb8523931a2a9b2b9
Ashwin Malayappan <ashwin.malayappan@gmail.com>
2026-09-21 10:32:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,7 +160,7 @@ if not st.session_state.finished:
 
                 st.session_state.current_question += 1  
 
-                st.session_state.adaptive_question = None
+                st.session_state.adaptive_question = question
 
                 st.session_state.answered = False
 
