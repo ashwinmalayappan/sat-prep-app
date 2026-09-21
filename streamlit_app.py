f890309a8b05bcb4c3f44c55f3dc72d2f1968cc2
Ashwin Malayappan <ashwin.malayappan@gmail.com>
2026-09-20 21:06:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ if "access_token" in st.session_state:
         st.session_state.refresh_token
     )
 
-st.write("Supabase user:", supabase.auth.get_user())
+st.write("Supabase session:", supabase.auth.get_session())
 
 # =========================
 # APP TITLE
